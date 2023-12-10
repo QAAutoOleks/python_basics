0e98317f93f4e2d4b08830a9baf6a027af450180
QA31Oleks <aleksandr.tsupko@gmail.com>
2023-12-10 15:22:14 +0200

    modified:   Selenium/find_element.py

--- Selenium/find_element.py
+++ Selenium/find_element.py
@@ -4,13 +4,14 @@ from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
 driver.get("https://www.example.com")
+
 time.sleep(3)
 
 # Get element with tag name 'div'
-element = driver.find_element(By.TAG_NAME, 'div')
+element = driver.find_element(By.TAG_NAME, "div")
 time.sleep(3)
 
 # Get all the elements available with tag name 'p'
-elements = element.find_elements(By.TAG_NAME, 'p')
+elements = element.find_elements(By.TAG_NAME, "p")
 for e in elements:
     print(e.text)
