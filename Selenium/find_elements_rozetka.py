b5bf31a9f05cdf9ae9b5ae64ceb5866254850da3
QA31Oleks <aleksandr.tsupko@gmail.com>
2023-12-10 15:59:39 +0200

    new file:   Selenium/find_elements_rozetka.py new file:   Selenium/get_attribut_rozetka.py

---
+++ Selenium/find_elements_rozetka.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.get("https://rozetka.com.ua/ua/")
+
+# If code below won't be deleted - 
+# NoSuchElementException
+# element = driver.find_element(By.CSS_SELECTOR, '[class="button button_color_green button_size_medium search-form__submit"]')
+# time.sleep(3)
+# print(element.text)
+
+time.sleep(3)
+elements = driver.find_elements(By.TAG_NAME, "a")
+# for e in elements:
+for e in elements:
+    print(e.text)
