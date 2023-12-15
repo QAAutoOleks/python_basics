cbe48e006f55a43d73acbce747c79e5a2007b078
Oleksandr Tsupko <aleksandr.tsupko@gmail.com>
2023-12-15 18:41:26 +0200

    modified:   PracticePythonHome/function.py add returning multiple values

--- PracticePythonHome/function.py
+++ PracticePythonHome/function.py
@@ -3,4 +3,19 @@ def hello(first_name, last_name, age):
     print("You are " + str(age) + " years old")
     print("Have a nice day!")
 
+
 hello("Bro", "Code", 21)
+
+
+# return multiple values from a function
+def calculation(a, b):
+    # addiction = a + b
+    # substraction = a - b
+    # return addiction, substraction
+    return a + b, a - b
+
+
+# return 'tuple'
+# print(calculation(20, 15))
+add, sub = calculation(40, 10)
+print(add, sub)
