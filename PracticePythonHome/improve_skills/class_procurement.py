ab240d15b25c6fef9f3553340c146b3f6de469d1
Oleksandr Tsupko <aleksandr.tsupko@gmail.com>
2023-12-20 14:17:13 +0200

    modified:   PracticePythonHome/improve_skills/class_procurement.py Add inheritance

--- PracticePythonHome/improve_skills/class_procurement.py
+++ PracticePythonHome/improve_skills/class_procurement.py
@@ -1,21 +1,37 @@
 class OpenProcedure:
     id_tender = 0
+    result_of_tender = ""
 
-    def __init__(self, name, classifier):
+    def __init__(self, name, classifier, expected_value):
         self.name = name
         self.classifier = classifier
+        self.expected_value = expected_value
+        OpenProcedure.result_of_tender = "Procedure is successful"
         OpenProcedure.id_tender += 1
 
     def __str__(self):
-        str_out = "Number of tender: " + str(OpenProcedure.id_tender) + "\nName of procurement: " + self.name + "\nClassifier: " + str(self.classifier) + "\n"
+        str_out = "Number of tender: " + str(OpenProcedure.id_tender)
+        str_out += "\nName of procurement: " + self.name + "\nClassifier: "
+        str_out += str(self.classifier) + "\n"
+        str_out += OpenProcedure.result_of_tender + "\n"
         return str_out
 
 
 class SimpleProcurement(OpenProcedure):
-    pass
+    
+    def __init__(self, name, classifier, expected_value):
+        self.name = name
+        self.classifier = classifier
+        self.expected_value = expected_value
+        OpenProcedure.id_tender += 1
+        if self.expected_value > 100000:
+            OpenProcedure.result_of_tender = "Procedure is not successful"
+        else:            
+            OpenProcedure.result_of_tender = "Procedure is successful"
 
-first_tender = OpenProcedure("Elektricity", 9004)
+
+first_tender = OpenProcedure("Elektricity", 9004, 150000)
 print(first_tender)
 
-simple_first_tender = SimpleProcurement("Сhemical reagent", 7005)
+simple_first_tender = SimpleProcurement("Сhemical reagent", 7005, 110000)
 print(simple_first_tender)
