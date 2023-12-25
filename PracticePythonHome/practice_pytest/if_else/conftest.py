72a7ced608050e2e6daec47a19918097b1e91881
Oleksandr Tsupko <aleksandr.tsupko@gmail.com>
2023-12-25 15:31:22 +0200

    modified:   PracticePythonHome/practice_pytest/if_else/conftest.py modified:   PracticePythonHome/practice_pytest/if_else/test_if_else.py

---
+++ PracticePythonHome/practice_pytest/if_else/conftest.py
@@ -0,0 +1,30 @@
+import pytest
+
+
+class IfElse:
+
+    def __init__(self, scores):
+        self.result = IfElse.scores_method(scores)
+
+    def scores_method(scores):
+        if scores <= 10 and scores >= 0:
+            return "D"
+        elif scores <= 20:
+            return "C"
+        elif scores <= 30:
+            return "B"
+        elif scores <= 40:
+            return "A"
+        else:
+            return "Incorrect input"
+
+    def remove_result(self):
+        self.result = ""
+
+@pytest.fixture
+def if_else_method():
+    if_else = IfElse(20)
+
+    yield if_else
+
+    if_else.remove_result()
---
+++ PracticePythonHome/practice_pytest/if_else/test_if_else.py
@@ -0,0 +1,5 @@
+import pytest
+
+
+def test_if_else(if_else_method):
+    assert if_else_method.result == 'C'
