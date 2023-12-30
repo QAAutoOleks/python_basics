e0cf0dcf3f1f5e2003cc5e340aad8864f0a26568
Oleksandr Tsupko <aleksandr.tsupko@gmail.com>
2023-12-30 16:34:46 +0200

    modified:   PracticePythonHome/improve_skills/assert_range_temperature.py

--- PracticePythonHome/improve_skills/assert_range_temperature.py
+++ PracticePythonHome/improve_skills/assert_range_temperature.py
@@ -1,7 +1,16 @@
 def water_condition(t):
     if t < 0:
         return "Ice"
-    elif t > 0 and t < 100:
+    elif t >= 0 and t < 100:
         return "Water"
     else:
         return "Vapor"
+
+# t = float(input("Enter temperature: "))
+
+assert water_condition(-1) == "Ice"
+assert water_condition(0) == "Water"
+assert water_condition(1) == "Water"
+assert water_condition(99) == "Water"
+assert water_condition(100) == "Vapor"
+assert water_condition(101) == "Vapor"
