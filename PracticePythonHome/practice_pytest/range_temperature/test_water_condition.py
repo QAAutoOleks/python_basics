635828ef31277438d0342eb0d4b94952895d67a4
Oleksandr Tsupko <aleksandr.tsupko@gmail.com>
2024-01-03 13:37:17 +0200

    modified:   PracticePythonHome/practice_pytest/range_temperature/__pycache__/test_water_condition.cpython-312-pytest-7.4.3.pyc modified:   PracticePythonHome/practice_pytest/range_temperature/pytest.ini modified:   PracticePythonHome/practice_pytest/range_temperature/test_water_condition.py

--- PracticePythonHome/practice_pytest/range_temperature/test_water_condition.py
+++ PracticePythonHome/practice_pytest/range_temperature/test_water_condition.py
@@ -1,13 +1,10 @@
 import pytest
 
-@pytest.mark.checking_all_values
+@pytest.mark.only_second
 def test_water(water_tests):
-        assert water_tests == [
-            "Ice", 
-            "Water", 
-            "Water", 
-            "Water",
-            "Vapor",
-            "Vapor",
-            "Vapor"
-        ]
+        assert water_tests[2] == "Water"
+
+
+@pytest.mark.only_first
+def test_water(water_tests):
+        assert water_tests[1] == "Water"
