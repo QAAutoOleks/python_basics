b470ffcd6f04c484bb92b761dd5112ba0350c7c1
Oleksandr Tsupko <aleksandr.tsupko@gmail.com>
2024-01-03 13:43:09 +0200

    modified:   PracticePythonHome/practice_pytest/range_temperature/__pycache__/test_water_condition.cpython-312-pytest-7.4.3.pyc modified:   PracticePythonHome/practice_pytest/range_temperature/test_water_condition.py modified:   PracticePythonHome/practice_pytest/value_2/__pycache__/conftest.cpython-312-pytest-7.4.3.pyc modified:   PracticePythonHome/practice_pytest/value_2/__pycache__/test_value.cpython-312-pytest-7.4.3.pyc

--- PracticePythonHome/practice_pytest/range_temperature/test_water_condition.py
+++ PracticePythonHome/practice_pytest/range_temperature/test_water_condition.py
@@ -1,10 +1,11 @@
 import pytest
 
+
 @pytest.mark.only_second
-def test_water(water_tests):
-        assert water_tests[2] == "Water"
+def test_water_second(water_tests):
+    assert water_tests[2] == "Water"
 
 
 @pytest.mark.only_first
-def test_water(water_tests):
-        assert water_tests[1] == "Water"
+def test_water_first(water_tests):
+    assert water_tests[1] == "Water"
