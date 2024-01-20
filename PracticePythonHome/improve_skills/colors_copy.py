6e366f5333f3528dedae4c0a2a4585323fcb313e
Oleksandr Tsupko <aleksandr.tsupko@gmail.com>
2024-01-20 12:29:08 +0200

    new file:   PracticePythonHome/improve_skills/colors_copy.py

---
+++ PracticePythonHome/improve_skills/colors_copy.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+driver = webdriver.Chrome()
+driver.get("http://google.com.ua")
+search_field = driver.find_element_by_name("q")
+search_field.send_keys("Ukraine")
+search_field.send_keys(Keys.RETURN)
+
+
+def add_numbers(a, b):
+    return a + b
+
+def test_add_numbers_positive():
+    assert add_numbers_positive(1, 2) == 3
+
+def test_add_numbers_negative():
+    assert add_numbers(-1, 2) == 1
