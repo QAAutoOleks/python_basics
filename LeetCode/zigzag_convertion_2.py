29dd94b5d60288e731744d0257fc8cfbfffd90bc
Oleksandr Tsupko <aleksandr.tsupko@gmail.com>
2024-02-05 14:57:41 +0200

    modified:   LeetCode/zigzag_convertion_2.py
    Attempt of solving zigzag conversion

--- LeetCode/zigzag_convertion_2.py
+++ LeetCode/zigzag_convertion_2.py
@@ -5,6 +5,7 @@ class Solution:
     def convert(self, s: str, numRows: int) -> str:
         arr = []
         s_editing = s
+        s_result = ""
         number_in_iteration = 0
         while(len(s_editing) > 0):
             if number_in_iteration == 0:
@@ -14,33 +15,18 @@ class Solution:
                 arr.append(s_editing[0:1])
                 s_editing = s_editing[1:len(s)]
             number_in_iteration += 1
-            if number_in_iteration == numRows:
-                number_in_iteration == 0
+            if number_in_iteration == numRows - 1:
+                number_in_iteration = 0
 
+        for i in range(0, len(arr)):
+            for j in range(0, len(arr)//numRows):
+                s_result += arr[j*numRows][i:i+1]
+        
         return arr
-
-    def quantity_of_cols(self, s, numRows):
-        counter_cols = 0
-        number_in_iteration = 0
-        len_str = len(s)
-        while(len_str > 0):
-            if number_in_iteration == 0:
-                len_str -= numRows
-                counter_cols +=1
-            else:
-                len_str -= 1
-                counter_cols +=1
-            number_in_iteration += 1
-            if number_in_iteration == numRows:
-                number_in_iteration == 0
-
-        return counter_cols
 
 
 solution = Solution()
 print(
     solution.convert(
-        "rrxtvodtzzduonaigmfdalyzeocxsmmmflfablvckbwyoxjvloalbamfppehdrvieblgmgiyhhxygivtwvfzvtgmikwndryisjqeradzhczpmujirqjojpbuzxhdohnjqdpkdulnykekgnszddnpsojsnsxeaknspecuznjxzoifbcehguwykfsyzrezdtusxwpwmywnmgvqizxqvtrgajgzdmbgfvzctobhozvdfqtnrsgnlxvnidmlppsukryghbnxaiafyvvqnbfyyangfasurmqcfoimsxlsgmaghvwxydvyflgknaeemugrlqfdorxwfzcoubluejskubuhbbloxuhimnnagnynmbbjcndiwyssbpzcqmsniayvpnxxawknxlybadjybeqctrhlgzyobyjsmjpmfzbenzfndqmguelnwsyetzsxzeplnfasgdytddhitvqqzbfxvgbrfwogadspkujrxhkcmtkhobxqedncjrtqpjwroqgzkpqiwckkwxrkapaeuqidhdvymrpdkvcumuekwpuumlfmahsuxdzgguevotayocscyxmwogrcswqufkrdnqlwnqtbjtbaxvcvuprixikpgckondravcyiurlgkoghkkeebypzizqpccdrfwtbaslvjxbwljfxvmczkrassqjwvonakhdnbpkmolkbwqztcbumuugonqlieaipjoekdoxrbhszzrsduprqjyfyosgssrjcfnmidlbettdunyyjnpayphxdzfyrwjvdxilcvohqimlxklgzciyspxxqcvibfdeensgjgpzqcmnoxwoagouylroppyquevarnictyemaqzoqxesesmcffsxurnqvkqozztvxxhzpiphguz",
-        399,
-    )
+        "PAYPALISHIRING", 3)
 )
