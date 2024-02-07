05bda155c428bab57023b253e1eccf81556960cf
Oleksandr Tsupko <aleksandr.tsupko@gmail.com>
2024-02-07 11:33:09 +0200

    modified:   LeetCode/string_to_integer_atoi.py
    bug fixing
    
    1073/1088 testcases

--- LeetCode/string_to_integer_atoi.py
+++ LeetCode/string_to_integer_atoi.py
@@ -1,17 +1,31 @@
 class Solution(object):
     def myAtoi(self, s):
         str_finish = ""
+        variable_for_str_characters = ""
         is_minus = False
+        is_plus_before = False
         for symbol in s:
-            if symbol.isnumeric():
+            if symbol == " ":
+                pass
+            elif symbol == "+" and len(str_finish) == 0 and is_minus == False:
+                is_plus_before = True
+            elif symbol.isnumeric():
                 str_finish += symbol
-            if symbol == "-":
+            elif symbol == "-" and is_plus_before == False:
                 is_minus = True
-        if is_minus == True:
+            else:
+                break
+        if is_minus == True and len(str_finish) > 0:
             str_finish = "-" + str_finish
         
-        return int(str_finish)
-
+        if len(str_finish) == 0:
+            return 0
+        elif int(str_finish) > 2147483648:
+            return 2147483647
+        elif int(str_finish) < -2147483648:
+            return -2147483648
+        else:
+            return int(str_finish)
 
 solution = Solution()
-print(solution.myAtoi("   -42"))
+print(solution.myAtoi("+-12"))
