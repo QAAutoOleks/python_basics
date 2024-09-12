ee0cf199be55ae750859ca3946d237b1e9691753
oleksandrtsupko <oleksandr.tsupko@gmail.com>
2024-09-12 21:08:44 +0300

    created initial test cases

--- LeetCode/reverse_integer.py
+++ LeetCode/reverse_integer.py
@@ -1,7 +1,16 @@
 class Solution(object):
     def reverse(self, x):
-        string_x = str(x)
-        return string_x
+
+        string_a = ""
+        x_abs = abs(x)
+        string_x = str(x_abs)
+
+        for a in string_x:
+            string_a = a + string_a
+        if x < 0:
+            string_a = "-" + string_a
+
+        return string_a
 
 
 solution = Solution()
