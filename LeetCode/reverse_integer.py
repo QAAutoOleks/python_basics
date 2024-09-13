c4aeb20b22326260445052b2de133fc9872b6db6
oleksandrtsupko <oleksandr.tsupko@gmail.com>
2024-09-13 20:11:48 +0300

    solution 13.09.24

--- LeetCode/reverse_integer.py
+++ LeetCode/reverse_integer.py
@@ -5,6 +5,15 @@ class Solution(object):
         x_abs = abs(x)
         string_x = str(x_abs)
 
+        i = 1
+
+        for a in range(len(string_x)-1, 0, -1):
+            if string_x[a] == 0:
+                i += 1
+            else:
+                break
+        string_x = string_x[0:len(string_x)-i]
+
         for a in string_x:
             string_a = a + string_a
         if x < 0:
@@ -14,4 +23,4 @@ class Solution(object):
 
 
 solution = Solution()
-print(solution.reverse(123))
+print(solution.reverse(-1230))
