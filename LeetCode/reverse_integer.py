04a4410d30bc87d001c67f981032a0468ac26260
oleksandrtsupko <oleksandr.tsupko@gmail.com>
2024-09-14 10:09:22 +0300

    Final solution

--- LeetCode/reverse_integer.py
+++ LeetCode/reverse_integer.py
@@ -5,7 +5,7 @@ class Solution(object):
         x_abs = abs(x)
         string_x = str(x_abs)
 
-        i = 1
+        i = 0
 
         for a in range(len(string_x)-1, 0, -1):
             if string_x[a] == 0:
@@ -19,8 +19,10 @@ class Solution(object):
         if x < 0:
             string_a = "-" + string_a
 
-        return string_a
+        if int(string_a) > pow(2, 31)-1 or int(string_a) < pow(-2, 31):
+            return 0
+        else: return int(string_a)
 
 
 solution = Solution()
-print(solution.reverse(-1230))
+print(solution.reverse(1230))
