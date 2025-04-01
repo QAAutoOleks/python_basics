d218f656d34a4ff609df71586631aa585f4cb509
oleksandrtsupko <oleksandr.tsupko@gmail.com>
2025-04-01 19:07:44 +0300

    Final solution

--- PracticePythonHome/improve_skills/merge_lists.py
+++ PracticePythonHome/improve_skills/merge_lists.py
@@ -1,4 +1,13 @@
-l1 = [1, 2, 3]
-l2 = [4, 5, 6]
+# l1 = [1, 2, 3]
+# l2 = [4, 5, 6]
 
-print(l1 + l2)
+# print(l1 + l2)
+
+def merge(lst1, lst2):
+    # Write code here
+    merged_list = lst1 + lst2
+    merged_list.sort()
+
+    return merged_list
+
+print(merge([1, 3], [2]))
