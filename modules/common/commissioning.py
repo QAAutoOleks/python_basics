3db9639f6a6669e6a1834c02f1081142c10b1cc3
oleksandrtsupko <oleksandr.tsupko@gmail.com>
2025-06-01 16:30:46 +0300

    test commissioning

--- modules/common/commissioning.py
+++ modules/common/commissioning.py
@@ -1,5 +1,12 @@
 class Commissioning:
 
+
     def addition(self):
         a = 1
         return a
+
+
+    def make_string(self, a, b):
+        string_sum = a + b
+
+        return string_sum
--- tests/test_mathematical.py
+++ tests/test_mathematical.py
@@ -3,3 +3,7 @@ import pytest
 @pytest.mark.commissioning_testing
 def test_check_correct_returning(addition_testing):
     assert addition_testing.addition() == 1
+
+@pytest.mark.commissioning_testing
+def test_sum_string(addition_testing):
+    assert addition_testing.make_string('Testing ', 'successful') == 'Testing successful'
