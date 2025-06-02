579f8bce1daef70323aa61132551ac524dc66029
oleksandrtsupko <oleksandr.tsupko@gmail.com>
2025-06-02 20:10:03 +0300

    test commissioning

--- modules/common/commissioning.py
+++ modules/common/commissioning.py
@@ -10,3 +10,11 @@ class Commissioning:
         string_sum = a + b
 
         return string_sum
+
+
+    def divisible_by_5_and_7(self, number_1):
+        incr = 0
+        for i in range(1, number_1):
+            if i % 7 == 0 and i % 5 == 0:
+                incr += 1
+        return incr
--- tests/test_mathematical.py
+++ tests/test_mathematical.py
@@ -1,9 +1,15 @@
 import pytest
 
-@pytest.mark.commissioning_testing
+@pytest.mark.do_not_doing
 def test_check_correct_returning(addition_testing):
     assert addition_testing.addition() == 1
 
-@pytest.mark.commissioning_testing
+
+@pytest.mark.do_not_doing
 def test_sum_string(addition_testing):
     assert addition_testing.make_string('Testing ', 'successful') == 'Testing successful'
+
+
+@pytest.mark.commissioning_testing
+def test_divisible_checking(addition_testing):
+    assert addition_testing.divisible_by_5_and_7(71) == 2
