523fd560a058195e8e6f0a46067b00cfc42c5792
oleksandrtsupko <oleksandr.tsupko@gmail.com>
2025-06-30 20:48:59 +0300

    Lesson 1 Soft Serve practice

---
+++ PracticePythonHome/calculating_mix.py
@@ -0,0 +1,18 @@
+a = float(input("Enter the first number (a):\n"))
+b = float(input("Enter the second number (b):\n"))
+
+sum_of_a_and_b = a + b
+subtraction = a - b
+multiplication = a * b
+division = a / b
+floor_division = a // b
+modulo = a % b
+raised_to_the_power = a**b
+
+print(f"a + b = {sum_of_a_and_b}")
+print(f"a - b = {subtraction}")
+print(f"a * b = {multiplication}")
+print(f"a / b = {division}")
+print(f"a // b = {floor_division}")
+print(f"a % b = {modulo}")
+print(f"a**b = {raised_to_the_power}")
