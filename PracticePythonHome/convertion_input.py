9a54076b38dbb869ad27d28c5699b344dd0d2801
oleksandrtsupko <oleksandr.tsupko@gmail.com>
2025-07-02 21:38:47 +0300

    modified:   PracticePythonHome/convertion_input.py

--- PracticePythonHome/convertion_input.py
+++ PracticePythonHome/convertion_input.py
@@ -5,3 +5,4 @@ height = float(input("How tall are you?: "))
 print("Hello " + name)
 print("You are " + str(age) + " years old")
 print("You are " + str(height) + "cm tall") 
+# The above code takes user input for name, age, and height, converts age to an integer and height to a float, and then prints the information back to the user.
